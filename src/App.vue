24f1652ebb8674c4b1b4d10b9bd00b6fe66b32c2
EVerardo Cunha <umbundu@gmail.com>
2019-03-10 11:04:19 -0400

    First commit without dist folder

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <input
+      v-model="message"
+      type="text">
+    
+      <h2 class="message">{{ message }}</h2>
+      <h3>One More Message</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World, universe',
+    };
+  },
+};
+</script>
+
+<style>
+.message {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color:blue;
+}
+</style>
